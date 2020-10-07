9da185accbe20cd80640b8cdfc8c976172879068
moli-debugger <mkmohamedalimoli@gmail.com>
2020-10-07 23:59:18 +0530

    1st

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-# This is a sample Python script.
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
 def print_hi(name):
@@ -9,7 +6,6 @@ def print_hi(name):
     print(f'Hi, I am {name} this code is all about extracting audiio from a video source')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('mohamed ali')
 
